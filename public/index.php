cdfdabe7268c7d90015ecd832d6b3ae0537991b0
Arthur Schneider <arthur.schneider@etudiant.univ-reims.fr>
2023-05-05 14:58:14 +0200

    fin de la partie 4.3

--- public/index.php
+++ public/index.php
@@ -1,3 +1,29 @@
 <?php
 
-echo 'Hello Music!';
+declare(strict_types=1);
+
+require_once '../vendor/autoload.php';
+
+use Database\MyPdo;
+
+MyPDO::setConfiguration('mysql:host=mysql;dbname=cutron01_music;charset=utf8', 'web', 'web');
+$html = <<<HTML
+<!DOCTYPE html>
+<title>liste artist</title>
+<html>
+HTML;
+
+$stmt = MyPDO::getInstance()->prepare(
+    <<<'SQL'
+    SELECT id, name
+    FROM artist
+    ORDER BY name
+SQL
+);
+
+$stmt->execute();
+
+while (($ligne = $stmt->fetch()) !== false) {
+    $html .= "<p>{$ligne['name']}\n";
+}
+echo $html;
